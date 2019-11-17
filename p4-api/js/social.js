224eb6ca6c2eb93d9097ee778524ca319527f637
Joan Xia <54558614+joanxiayiqiong@users.noreply.github.com>
2019-11-17 12:16:47 -0500

    Update social.js

--- p4-api/js/social.js
+++ p4-api/js/social.js
@@ -7,7 +7,7 @@ $( document ).ready(function() {
 function loadData(){
   $.ajax({
     type:"GET",
-    url:"../twitter-bot/tweets.json",
+    url:"twitter-bot/tweets.json",
     dataType:"json",
     success:parseData
   });
